70b4ce6346fa91cdd92d36ea4277721c21583151
Shuzann Hoh <shuzannh@student.unimelb.edu.au>
2019-09-28 23:06:10 +1000

    added basic menu and audio player

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource audioPlayer;
+
+    private bool isPlaying = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        audioPlayer.Play(0);
+    }
+
+    public void ToggleMusicStop()
+    {
+        if (isPlaying){
+            audioPlayer.Stop();
+        }
+        else{
+            audioPlayer.Play(0);
+        }
+
+        isPlaying = !isPlaying;
+    }
+
+    public void ToggleMusicPause()
+    {
+        if (isPlaying){
+            audioPlayer.Pause();
+        }
+        else{
+            audioPlayer.UnPause();
+        }
+
+        isPlaying = !isPlaying;
+    }
+}
--- Assets/Scripts/BobMovement.cs
+++ Assets/Scripts/BobMovement.cs
@@ -6,12 +6,13 @@ public class BobMovement : MonoBehaviour
 {
     public float speed = 0.5f;
     public float height = 0.5f;
+    public float delay;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.localPosition;
-        float newY = Mathf.Sin(Time.time * speed);
+        float newY = Mathf.Sin((Time.time * speed)+delay);
         transform.localPosition = new Vector3(pos.x, newY, pos.z) * height;
         
     }
