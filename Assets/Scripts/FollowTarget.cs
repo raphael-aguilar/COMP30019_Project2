1d1baf0582c22e3cb1b012b6af3a3aaa11e66b22
Raphael Aguilar <raphaela@student.unimelb.edu.au>
2019-10-09 14:56:18 +1100

    test level, following camera added

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public GameObject player;
+    
+    public float height;
+    private float x_dist = 0;
+    private float z_dist = 5;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 position = new Vector3(player.transform.position.x - x_dist, height, player.transform.position.z - z_dist);
+        this.transform.position = position;
+    }
+}
